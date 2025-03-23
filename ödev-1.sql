ae9107d054fc08f96d9e1b97d8b31d39bf363176
Mehmet Emre Ünlü <147059488+Memreu@users.noreply.github.com>
2025-03-23 15:47:54 +0300

    Update and rename ödev-1.txt to ödev-1.sql

--- ödev-1.sql
+++ ödev-1.sql
@@ -4,21 +4,21 @@ SORU 1                                         |
 SELECT FirstName, LastName, Salary
 FROM Employees;
 
-From komutunu ile hangi tabloyu kullanacağımızı belirleyip select ile istediğimiz bilgileri çekiyoruz.
+-- From komutunu ile hangi tabloyu kullanacağımızı belirleyip select ile istediğimiz bilgileri çekiyoruz.
 ************************************************
 SORU 2                                         |
 ------------------------------------------------
 SELECT Distinct departmentid
 FROM Employees;
 
-distinct komutu ile tüm departmanları yazdırdım.
+-- distinct komutu ile tüm departmanları yazdırdım.
 ************************************************
 SORU 3                                         |
 ------------------------------------------------
 SELECT * FROM Employees
 WHERE departmentid = 1;
 
-WHERE komutu ile belli bir departmentidye sahip olanları seçtim. Tabloya göre IT tarafının id'si 1
+-- WHERE komutu ile belli bir departmentidye sahip olanları seçtim. Tabloya göre IT tarafının id'si 1
 ************************************************
 SORU 4                                         |
 ------------------------------------------------
@@ -26,11 +26,11 @@ SELECT FirstName, LastName, Salary
 FROM Employees
 ORDER BY Salary DESC;
 
-Desc ile büyükten küçüğe göre sıraladım.
+-- Desc ile büyükten küçüğe göre sıraladım.
 ************************************************
 SORU 5                                         |
 ------------------------------------------------
 SELECT FirstName || ' ' || LastName AS Tümisim, Salary
 FROM Employees;
 
- || ile sütunları birleştirip ismini Tümisim koydum.
+--  || ile sütunları birleştirip ismini Tümisim koydum.
